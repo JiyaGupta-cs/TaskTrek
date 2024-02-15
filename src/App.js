b31596844e7117458267f571688cd700b25e03d0
Jiya <coderaholic21@gmail.com>
2024-02-16 01:55:34 +0530

    Added Tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { FaTrashCan } from "react-icons/fa6";
 
-function App() {
+export default function App() {
+  const [items, setItems] = useState([
+    { id: 1, name: "Buy Grocery", checked: true },
+    { id: 2, name: "Make a TODO", checked: false },
+    { id: 3, name: "Update today's assignments", checked: true },
+  ]);
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id == id ? { ...item, checked: !item.checked } : item,
+    );
+    setItems(listItems);
+  };
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="flex justify-center items-center h-screen">
+      <section className=" w-3/5">
+      <ul className="flex flex-col gap-4">
+        {items.map((item) => (
+          <li className="bg-gray-300 p-4 flex justify-between items-center" key={item.id}>
+            <input
+            className="w-5 h-5"
+              onChange={() => {
+                handleCheck(item.id);
+              }}
+              checked={item.checked}
+              type="checkbox"
+            />
+            <label
+            className="text-lg"
+              style={(item.checked)?{textDecoration:'line-through'}:null}
+              >{item.name}</label>
+
+            <FaTrashCan className="w-5 h-5 text-blue-600 hover:text-red-600 hover:w-6 hover:h-6 ease-in duration-200" role="button" tabIndex="0" />
+          </li>
+        ))}
+      </ul>
+      </section>
+     
+    </main>
   );
 }
-
-export default App;
