f1b9043b0639943f4bf4d10fd4700282499c3d41
JiyaGupta-cs <140608790+JiyaGupta-cs@users.noreply.github.com>
2024-02-15 17:50:30 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
+import { ListFormat } from "typescript";
 import "./App.css";
 import { useState } from "react";
 
 export default function App() {
-  const name = useState("Rohit");
-
-  const [count, setCount] = useState(0);
-
-  const handleClick = () => {
-    setCount(count + 1);
-    setCount(count + 1);
-    console.log(count);
-  };
-  const printClick = () => {
-    console.log(count);
-  };
+  const [items, setItem] = useState([
+    { id: 1, name: "Buy Grocery", checked: true },
+    { id: 2, name: "Make a TODO", checked: false },
+    { id: 3, name: "Update today's assignments", checked: true },
+  ]);
 
   return (
     <main>
-      <h1>Hello {name}</h1>
-      <button onClick={handleClick}>Update+1</button>
-      <button onClick={printClick}>Print in Console</button>
+      <ul>
+        {items.map((item) => (
+      <li>
+      <input checked={item.checked} type="checkbox"/>
+      <label>{item.name}</label>
+        </li>
+        ))}
+        
+      </ul>
     </main>
   );
 }
